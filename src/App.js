9b0871b3c6c8ca973950285306a7d6c7fe4af563
Shashank S. Khasare <shashank.khasare@gmail.com>
2020-03-22 16:02:39 +0530

    Add material-table

--- src/App.js
+++ src/App.js
@@ -1,62 +1,37 @@
 import React from "react";
-import "./styles.css";
+import MaterialTable from "material-table";
+import axios from "axios";
 
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+export default class App extends React.Component {
+  state = {
+    accounts: [],
+    columns: [
+      "Account No",
+      "Date",
+      "Transaction Details",
+      "Value Date",
+      "Withdrawal AMT",
+      "Deposit AMT",
+      "Balance AMT"
+    ]
+  };
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650
+  componentDidMount() {
+    axios.get(`http://starlord.hackerearth.com/bankAccount`).then(res => {
+      const accounts = res.data;
+      console.log(res);
+      console.log(accounts);
+      this.setState({ accounts });
+    });
   }
-});
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9)
-];
-
-export default function App() {
-  const classes = useStyles();
-
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  render() {
+    return (
+      <MaterialTable
+        title="Editable Example"
+        columns={this.state.columns}
+        data={this.state.accounts}
+      />
+    );
+  }
 }
